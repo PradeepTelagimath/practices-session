d678338c34d85c62d0c32c6b83ea9a8e193d5ef7
Pradeep Telagimath <pradeeptelagimath@gmail.com>
2026-08-04 17:00:45 +0530

    prac

---
+++ positive or negative.py
@@ -0,0 +1,7 @@
+a = int(input())
+
+if a>0:
+    print("postive")
+else:
+    print("negative")
+
---
+++ prime or not.py
@@ -0,0 +1,11 @@
+a = int(input())
+
+if a<2:
+    print("not prime")
+else:
+    for i in range(2,a):
+        if a%i == 0:
+            print("not prime")
+            break
+    else:
+        print("prime")
